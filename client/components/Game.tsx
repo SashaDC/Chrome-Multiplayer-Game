92fa9e3515dd9509578a8f2884e3e9067d50554d
Sasha Chambers <sashachambers10.12@gmail.com>
2025-08-06 00:12:01 +1200

    Made a 'usable' avatar/character, click to move, movement animation

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -1,13 +1,40 @@
-interface Props {
+import { useState } from 'react'
+
+interface Name {
   displayName: string
 }
 
-export default function Game({ displayName }: Props) {
+interface Position {
+  x: number
+  y: number
+}
+
+export default function Game({ displayName }: Name) {
+  /* Sets the positions to null by default so user spawns in the middle of the game */
+  const [position, setPosition] = useState<Position | null>(null)
+
+  /* This finds the x and y position of where the mouse clicked and sets the useState position to them */
+  const handleClick = (event: React.PointerEvent<HTMLDivElement>) => {
+    setPosition({ x: event.clientX, y: event.clientY })
+  }
+
+  {
+    /*if a person clicks anywhere in the Game-Container div it triggers handleclick*/
+  }
   return (
-    <div className="Game-Container">
-      <h1>Game</h1>
-      <div className="Player">
-        <div className="Avatar"></div>
+    <div className="Game-Container" onClick={handleClick}>
+      <div
+        className="Player"
+        style={
+          /* If the position of both x/y dont equal null use the useState positions otherwise undefined */
+          position?.x != null && position?.y != null
+            ? { left: position.x, top: position.y }
+            : undefined
+        }
+      >
+        <div className="Avatar">
+          <img src="funny.png" alt="penguin" className="penguin" />
+        </div>
         <div className="Display-Name">{displayName}</div>
       </div>
     </div>
